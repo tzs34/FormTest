b2fafe5ca4c236d98e156214bfbe371994594d61
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-09-17 14:00:20 +0100

    Tidied up code

--- src/components/styled-components/components.js
+++ src/components/styled-components/components.js
@@ -4,30 +4,33 @@ import styled from 'styled-components'
 export const FormContainer = styled.div`
     margin-top: 20px;
     padding: 10px;
-`
+`;
+
 export const FormSubHeader = styled.h3`
     width: calc(100% - 200);
     text-align: center;
     padding: 8px;
     border-bottom: 1px solid  rgb(63, 81, 181);
-`
+`;
+
 export const FormHeader = styled.h2`
     text-align: center;
     padding: 8px;
-`
+`;
 
 export const Row = styled.div`
     display: flex;
     justify-content: ${props => props.justifyContent || 'center'};
     align-items: ${props => props.alignItems || 'center'};
     width: 100%;
-`
+`;
+
 export const WrapRow = styled.div`
   	margin: 0;
 	display: flex;
 	flex-flow: row wrap;
 	justify-content: flex-start;
-`
+`;
 
 export const DatePickerContainer = styled.div`
     display: flex;
@@ -49,16 +52,17 @@ export const TwoFieldContainer = styled.div`
         margin: 0.25em;
         flex-direction: column;
     }
-`
+`;
 
 export const CardBox = styled.div`
     box-shadow: 1px 3px #d4d4d5;
     padding: 0.5rem;
     width: 130px;
     margin: 1rem;
-`
+`;
+
 export const CardLabel = styled.label`
     font-size: 1em;
     font-weight: bold;
     margin: 1em;
-`
+`;
